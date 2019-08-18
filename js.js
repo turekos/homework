2f0b67b1ac0484336278e254131559bd2684428c
olo <tureksasha@gmail.com>
2019-08-18 20:27:44 +0200

    done. task 3

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
-'use strict';
-let money = +prompt("Budget for a month", " ");
-let time = prompt("Enter date please YYYY-MM-DD", "");
+"use strict";
+let money = +prompt("Budget for a month", " "),
+    time = prompt("Enter date please YYYY-MM-DD", "");
 
 let appData = {
     budget: money,
@@ -10,10 +10,51 @@ let appData = {
     income: [],
     savings: false
 };
-let requiredItem = prompt("Enter the required item this month", " ");
-let costOfItem = +prompt("How much it costs", " ");
 
-appData.expenses[requiredItem] = costOfItem;
+
+/*for (let i = 0; i < 2; i++) {
+    let requiredItem = prompt("Enter the required item this month", " "),
+        costOfItem = +prompt("How much it costs", " ");
+    if ((typeof (requiredItem)) == 'string' && (typeof (requiredItem)) != null &&
+        (typeof (costOfItem)) != null && requiredItem != " " && costOfItem != " " && requiredItem.length < 50) {
+        console.log('good');
+        appData.expenses[requiredItem] = costOfItem;
+    } else {
+        console.log("one more time");
+        i = i - 1;
+    }
+
+}*/
+let i = 0;
+
+
+do {
+    let requiredItem = prompt("Enter the required item this month", " "),
+        costOfItem = +prompt("How much it costs", " ");
+    if ((typeof (requiredItem)) == 'string' && (typeof (requiredItem)) != null &&
+        (typeof (costOfItem)) != null && requiredItem != " " && costOfItem != " " && requiredItem.length < 50) {
+        console.log('good');
+        appData.expenses[requiredItem] = costOfItem;
+    } else {
+        console.log("one more time");
+        i = i - 1;
+    }
+    i++;
+} while (i < 2);
+
+
+
+
+appData.moneyPerDay = appData.budget / 30;
+if (appData.moneyPerDay < 100) {
+    console.log("Low");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("MIdlle");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Hight");
+} else {
+    console.log("Error");
+}
 
 
 
